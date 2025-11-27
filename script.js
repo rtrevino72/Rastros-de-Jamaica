6c0e9fce485398d051e70f4b4664c93e41339569
Sarah <ssantamaria@centro.edu.mx>
2025-11-27 12:22:10 -0600

    asesoria

--- script.js
+++ script.js
@@ -79,3 +79,18 @@ if (introVideo) {
     introOverlay.addEventListener("click", finishIntro);
   }
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
