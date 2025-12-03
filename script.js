40473b8cc480584f3ddc625400e85cf8c3664d16
Sarah <ssantamaria@centro.edu.mx>
2025-12-03 10:16:42 -0600

    fotos

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const introVideo = document.querySelector(".intro-video");
 if (introVideo) {
     introVideo.addEventListener("loadeddata", () => {
         document.body.classList.add("video-loaded");
+       
+        // --- Slider de fotos (Swiper) ---
+  const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    });
     });
 }
 
